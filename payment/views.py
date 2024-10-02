544b2c22c3d780b71f4e2ebb37ab649fc6eb5584
Jisung Smart <jisung.smart@gmail.com>
2024-10-03 06:03:40 +0900

    payment 에 view 함수와 템플릭 코드 적용

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404,redirect
+from django.conf import settings
+from django.urls import reverse
+from orders.models import Order
 
-# Create your views here.
+
+def payment_process(request):
+    return render(request, 'payment/process.html')
