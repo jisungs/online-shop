5c21d0a3598ffbf1a0706eb62e204513f6383483
Jisung Smart <jisung.smart@gmail.com>
2024-10-11 04:36:25 +0900

    update payment app urls.py

--- payment/urls.py
+++ payment/urls.py
@@ -4,9 +4,7 @@ from . import views
 app_name = 'payment'
 
 urlpatterns = [
-    path('pay/',views.payment_request, name='payment_request'),
-    path('pay/check/<str:merchant_uid>/', views.check_payment, name='check_payment'),
-    path('pay/verify/<int:product_price>/<str:merchant_uid>/', views.verify_payment, name='verify_payment'),
-    path('pay/cancel/<str:merchant_uid>/', views.cancel_payment, name='cancel_payment'),
-    path('pay/complete', views.payment_complete, name='payment_complete')
+    path('process/', views.payment_process, name='process'),
+    path('completed/', views.payment_completed, name='completed'),
+    path('canceled/', views.payment_canceled, name='canceled'),
 ]
