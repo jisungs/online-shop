67a631ec6966f1cfd80674d00b8aed137b7d974e
Jisung Smart <jisung.smart@gmail.com>
2024-10-28 05:03:38 +0900

    주문 상세 페이지 추가

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,8 @@ import datetime
 
 from django.contrib import admin
 from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from .models import Order, OrderItem
 
@@ -41,6 +43,12 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = [
@@ -54,7 +62,9 @@ class OrderAdmin(admin.ModelAdmin):
         'paid',
         'created',
         'updated',
+        order_detail,
     ]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
+
